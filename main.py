53968bad91da3db016bdd6d492c952f1577de7dd
Jay Mangi <32821987+Jay2201@users.noreply.github.com>
2023-09-12 17:47:11 +0530

    small change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! Demo for CD'
+    return 'Hello World for GCP Vertex AI'
 
 @app.route('/echo/<name>')
 def echo(name):
