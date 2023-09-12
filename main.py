179c6f30470021fb0beb193e12af71a0e8219478
Jay Mangi <32821987+Jay2201@users.noreply.github.com>
2023-09-12 12:39:26 +0530

    small change
    on push

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CICD'
+    return 'Hello World! Demo for CD'
 
 @app.route('/echo/<name>')
 def echo(name):
